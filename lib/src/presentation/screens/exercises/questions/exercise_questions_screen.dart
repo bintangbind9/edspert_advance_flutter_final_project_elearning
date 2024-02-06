8eba8025632082f05c7a475f5d3f6c0093e2ccb8
bintangbind9 <bintangsaputra531@gmail.com>
2024-02-07 06:12:32 +0700

    Enhancement Exercise Questions Screen

--- lib/src/presentation/screens/exercises/questions/exercise_questions_screen.dart
+++ lib/src/presentation/screens/exercises/questions/exercise_questions_screen.dart
@@ -180,18 +180,34 @@ class _ExerciseQuestionsScreenState extends State<ExerciseQuestionsScreen> {
                               },
                             )
                           : const SizedBox(),
-                      CommonButton(
-                        text: questionIndex < questionsCount - 1
-                            ? 'Selanjutnya'
-                            : 'Kumpulin',
-                        onPressed: () {
-                          if (questionIndex < questionsCount - 1) {
-                            context.read<QuestionIndexBloc>().add(
-                                SetQuestionIndexEvent(
-                                    index: questionIndex + 1));
-                          } else {
-                            Fluttertoast.showToast(msg: 'Kumpulin');
-                          }
+                      BlocBuilder<QuestionAnswerBloc, QuestionAnswerState>(
+                        builder: (context, state) {
+                          return CommonButton(
+                            text: questionIndex < questionsCount - 1
+                                ? 'Selanjutnya'
+                                : 'Kumpulin',
+                            onPressed: () {
+                              if (questionIndex < questionsCount - 1) {
+                                context.read<QuestionIndexBloc>().add(
+                                    SetQuestionIndexEvent(
+                                        index: questionIndex + 1));
+                              } else {
+                                if (state is SetQuestionAnswerSuccess) {
+                                  if (state.questionAnswers.any(
+                                    (e) =>
+                                        e.answer == GeneralValues.defaultAnswer,
+                                  )) {
+                                    Fluttertoast.showToast(
+                                      msg:
+                                          'Oops! Soal belum dikerjakan semua. Periksa kembali jawaban Kamu!',
+                                    );
+                                  } else {
+                                    Fluttertoast.showToast(msg: 'Kumpulin');
+                                  }
+                                }
+                              }
+                            },
+                          );
                         },
                       ),
                     ],
